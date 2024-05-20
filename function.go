95c3b54c485e7445999c352cdcdfe827172b4a15
tompeq <ivan120137@mail.ru>
2024-05-20 21:30:05 +1000

    Added new function

---
+++ function.go
@@ -0,0 +1,19 @@
+func sumNumbersFromFile(filename string) int {
+    // Здесь обычно идет код, который загружает содержимое файла filename
+    // Например, используя ioutil.ReadFile(filename) или другие способы
+    
+    // Для примера, создадим слайс чисел для имитации данных из файла
+    numbers := []string{"1", "2", "3", "4", "5"}
+
+    sum := 0
+    for _, numStr := range numbers {
+        num, err := strconv.Atoi(numStr)
+        if err != nil {
+            fmt.Println("Ошибка преобразования числа:", err)
+            continue
+        }
+        sum += num
+    }
+
+    return sum
+}
